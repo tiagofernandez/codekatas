95965a962722f3f76d8c7451e7fe4a47436122a8
Tiago Fernandez <tiago.fernandez@gmail.com>
2010-12-28 21:50:34 +0100

    Finished initial implementation.

--- matchmaker/lib/matchmaker.rb
+++ matchmaker/lib/matchmaker.rb
@@ -1,7 +1,55 @@
 class MatchMaker
   
   def get_best_matches(members, current_user, sf)
-    []
+    matches = {}
+    current_member = find_member(members, current_user)
+    current_answers = get_answers(current_member)
+    members.each do |member|
+      if potential_match?(current_member, member)
+        answers = get_answers(member)
+        count = count_matches(current_answers, answers)
+        matches[get_name(member)] = count if count >= sf
+      end
+    end
+    return matches.sort { |a, b| -(a[1] <=> b[1]) }.collect { |elem| elem[0] }
   end
   
+  private
+
+  def potential_match?(member_a, member_b)
+    get_date(member_a) == get_gender(member_b) and get_gender(member_a) == get_date(member_b)
+  end
+
+  def find_member(members, name)
+    result = members.select { |member| get_name(member) == name }
+    raise "Member not found: #{name}" if result.length == 0
+    raise "Duplicated member: #{name}" if result.length > 1
+    return result[0]
+  end
+  
+  def count_matches(expected_answers, actual_answers)
+    count = 0
+    expected_answers.each_index do |i|
+      count += 1 if expected_answers[i] == actual_answers[i]
+    end
+    return count
+  end
+  
+  def get_name(member)
+    member.split[0]
+  end
+  
+  def get_gender(member)
+    member.split[1]
+  end
+  
+  def get_date(member)
+    member.split[2]
+  end
+  
+  def get_answers(member)
+    tokens = member.split
+    tokens[3..tokens.length]
+  end
+    
 end
--- matchmaker/spec/matchmaker_spec.rb
+++ matchmaker/spec/matchmaker_spec.rb
@@ -4,10 +4,22 @@ describe MatchMaker do
   
   before :all do
     @matchmaker = MatchMaker.new
+    @members = [
+      "BETTY F M A A C C",
+      "TOM M F A D C A",
+      "SUE F M D D D D",
+      "ELLEN F M A A C A",
+      "JOE M F A A C A",
+      "ED M F A D D A",
+      "SALLY F M C D A B",
+      "MARGE F M A A C C"
+    ]
   end
   
   it "should get best matches" do
-    @matchmaker.get_best_matches([], '', -1).should == []
+    @matchmaker.get_best_matches(@members, 'BETTY', 2).should == ['JOE', 'TOM']
+    @matchmaker.get_best_matches(@members, 'JOE', 1).should == ['ELLEN', 'BETTY', 'MARGE']
+    @matchmaker.get_best_matches(@members, 'MARGE', 4).should == []
   end
 
 end
